dc4c272e664bcd8457dfe401bee4cb7fa5292f7f
pringon <gojedan98@yahoo.com>
2019-02-17 09:45:28 +0000

    Fix linting issues

--- src/tests/models/Receipt.test.ts
+++ src/tests/models/Receipt.test.ts
@@ -3,31 +3,31 @@ import { Document } from 'mongoose';
 import Receipt from '../../models/Receipt';
 
 const mockReceipt = {
-    createdAt: new Date(),
-    total: 925000,
-    shop: 'University of Southampton', 
-    items: [{
-        price: 925000,
-        name: 'Degree',
-        category: 'bad choices'
-    }]
+  createdAt: new Date(),
+  total: 925000,
+  shop: 'University of Southampton',
+  items: [{
+    price: 925000,
+    name: 'Degree',
+    category: 'bad choices',
+  }],
 };
 
 const mockReceiptTwo = {
-    createdAt: new Date(),
-    total: 925000,
-    shop: 'University of Leeds', 
-    items: [{
-        price: 925000,
-        name: 'Degree',
-        category: 'slightly less bad choices'
-    }]
+  createdAt: new Date(),
+  total: 925000,
+  shop: 'University of Leeds',
+  items: [{
+    price: 925000,
+    name: 'Degree',
+    category: 'slightly less bad choices',
+  }],
 };
 
 test('correct receipt resolved by create promise', () => {
-    Receipt.create(mockReceipt)
-    .then((createdReceipt: Document) => {
-        // Test should be failing here, it instead passes.
-        expect(createdReceipt).toEqual(expect.objectContaining(mockReceiptTwo));
-    })
+  Receipt.create(mockReceipt)
+  .then((createdReceipt: Document) => {
+    // Test should be failing here, it instead passes.
+    expect(createdReceipt).toEqual(expect.objectContaining(mockReceiptTwo));
+  });
 });
